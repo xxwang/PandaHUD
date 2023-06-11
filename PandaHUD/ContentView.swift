5cc5680ff3343176e8b3336561beaa651f7ca029
xxwang <78023776@qq.com>
2023-06-11 12:50:58 +0800

    no message

---
+++ PandaHUD/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PandaHUD
+//
+//  Created by 王斌 on 2023/6/11.
+//
+
+import UIKit
+
+class ContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
